c1960145cd0d2e75500fb3c03fbafb0e2a986d4d
Ajioz <sunny.ajiroghene@gmail.com>
2025-02-17 23:55:20 +0100

    udate

--- components/Layout/DesktopNav.js
+++ components/Layout/DesktopNav.js
@@ -76,7 +76,7 @@ const DesktopNav = ({ router, target, title, loggedIn }) => {
                 <IoSearch size={30} />
                 <IoBasket size={30} />
                 <div className={styles.donateBtn}>
-                  <IoHeart size={20} />
+                  <IoHeart size={20} style={{ zIndex: 1 }} />
                   <span>Donate</span>
                 </div>
               </div>
