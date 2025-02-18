01f920ab8f7d26f690b75b6e267979aa90aedb34
Ajioz <sunny.ajiroghene@gmail.com>
2025-02-18 10:04:55 +0100

    fixed img bck noise

--- components/Home/Report/index.js
+++ components/Home/Report/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import report from "./report.module.css";
-import Image from "next/image";
 
 const Report = () => {
   return (
     <div className={report.container}>
-      {/* <Image src="/images/main/report.webp" width={1920} height={293} alt="report" /> */}
     </div>
   );
 };
